3c91d4d5eb8b6501f36b46fc66ce18e56a4b0ad6
Manuel Fidalgo <m.fidalgo.fierro@gmail.com>
2016-12-30 13:11:28 +0100

    Started project, implementing the main page using buttons and a gridlayout

---
+++ PrincipalPage.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Safe
+{
+    public class MainPage : ContentPage
+    {
+   
+        public MainPage()
+        {
+            var top = new Button
+            {
+                Text = "top",
+                BackgroundColor = new Color(127, 0, 0)
+            };
+
+            var middle = new Button
+            {
+                Text = "middle",
+                BackgroundColor = new Color(0, 127, 0)
+            };
+
+            var bottom = new Button
+            {
+                Text = "Bottom",
+                BackgroundColor = new Color(0, 0, 127)
+            };
+
+            addEventHandlers(top, middle, bottom);
+
+            var layout = new Grid
+            {
+                
+                Padding = 40,
+                Children = { top, middle, bottom }
+            };
+
+            Content = layout;
+            
+        }
+
+        private void addEventHandlers(Button top, Button middle, Button bottom)
+        {
+            top.Clicked += TopClicked;
+            middle.Clicked += MiddleClicked;
+            bottom.Clicked += BottomClicked;
+        }
+        
+        //Clicked events
+        private void TopClicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Info Message", "top", "Ok");
+        }
+
+        private void MiddleClicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Info Message", "middle", "Ok");
+        }
+
+        private void BottomClicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Info Message", "bottom", "Ok");
+        }
+    }
+}
